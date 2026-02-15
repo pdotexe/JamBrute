cff2bfcbdc96d075a90c9ec8a5b170be272c73a0
pdotexe <rehanchahal08@gmail.com>
2026-02-15 00:36:49 -0500

    Initial Commit

--- src/wordlist.py
+++ src/wordlist.py
@@ -1,26 +1,7 @@
 from colorama import Fore
-import gzip
-import shutil
-import os
-
-
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-source_file = os.path.join(project_root, "txt", "rockyou.txt.gz")
-destination_file = os.path.join(project_root, "txt", "rockyou.txt")
 
 
 
-try:
-    print(Fore.YELLOW + f"Unzipping {source_file} to {destination_file}...")
-    with gzip.open(source_file, 'rb') as file_in:
-        with open(destination_file, 'wb') as file_out:
-            shutil.copyfileobj(file_in, file_out)
-except FileNotFoundError:
-    print(Fore.RED + f"Error: {source_file} not found.")
-    exit(1)
-except Exception as e:
-    print(Fore.RED + f"Error: {e}")
-    exit(1)
 
 class Wordlist:
 
@@ -29,7 +10,7 @@ class Wordlist:
         
 
     def open_wordlist(self) -> list[str]:
-        with open(destination_file, 'r', encoding='utf-8', errors='ignore') as file:
+        with open("txt/rockyou.txt", 'r', encoding='utf-8', errors='ignore') as file:
             for line in file:
                 stripped_line = line.strip()
                 if stripped_line:
