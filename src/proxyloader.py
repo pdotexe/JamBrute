c161c59205cd61d83873f61ec6be9b74995985f1
pdotexe <rehanchahal08@gmail.com>
2026-02-16 09:53:15 -0500

    Initial Commit

--- src/proxyloader.py
+++ src/proxyloader.py
@@ -11,11 +11,11 @@ class ProxyLoader:
         self.proxy_list: list[str] = []
         self.test = "http://httpbin.org/status/200" # proxy test endpoint
     
-    def load_proxies(self,) -> list[str]:
+    def load_proxies(self) -> list[str]:
         try:   
-            with open("txt/proxies.txt", 'r') as p:
+            with open("txt/proxies.txt", 'r') as p: 
                 for l in p:
-                    proxy = l.strip()
+                    proxy = l.strip() # strip whitespace
                     if proxy:
                         try:
                             if not proxy.startswith("socks5://") and not proxy.startswith("http://") and not proxy.startswith("https://"):
@@ -24,7 +24,7 @@ class ProxyLoader:
                                 print(Fore.RED + f'Proxy {proxy} is not a valid proxy protocol, skipping.')
                                 continue
                             proxyresponse = requests.head(self.test, proxies={"http": proxy, "https": proxy}, timeout=2.5)
-                            if proxyresponse.status_code in [200, 301, 302, 304, 405]:
+                            if proxyresponse.status_code in [200, 301, 302, 304, 405]: #http status check
                                 self.proxy_list.append(proxy)
                                 print(Fore.GREEN + f'Proxy {proxy} working with HTTP/{proxyresponse.status_code})')
                             else:
